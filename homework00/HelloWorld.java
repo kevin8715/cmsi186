ab00e282ee096bd8524346bb3c3369b864c6e042
kevin8715 <ksolis01@att.net>
2018-01-15 23:48:59 -0800

    Creating HelloWorld.Java
    Tested and header included

---
+++ homework00/HelloWorld.java
@@ -0,0 +1,23 @@
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  HelloWorld.java
+ *  Purpose       :  Says "Hello world", template for basic structure that all java programs need
+ *  Author        :  Kevin Solis Sosa
+ *  Date          :  2017-01-15
+ *  Description   :  See purpose
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-02  B.J. Johnson  Initial writing and release
+ *  @version 1.0.1  2017-12-25  B.J. Johnson  Updated for Spring 2018 version
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class HelloWorld {
+	public static void main( String[] args ) {
+		System.out.println( "Hello World!" );
+	}
+}
