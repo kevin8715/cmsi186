db069e65ec1198ed05eb144509cbe05d7c0a67fa
kevin8715 <ksolis01@att.net>
2018-02-21 18:57:47 -0800

    Adding Partially complete files
    Need debugging and renaming

---
+++ homework03/HighRoll.java
@@ -0,0 +1,61 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  DiceSet.java
+ *  Purpose       :  Provides a class describing a set of dice
+ *  Author        :  B.J. Johnson
+ *  Date          :  2017-02-09
+ *  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
+ *                   idea here is to have an implementing class that uses the Die.java class.  Includes
+ *                   the following:
+ *                   public DiceSet( int k, int n );                  // Constructor for a set of k dice each with n-sides
+ *                   public int sum();                                // Returns the present sum of this set of dice
+ *                   public void roll();                              // Randomly rolls all of the dice in this set
+ *                   public void rollIndividual( int i );             // Randomly rolls only the ith die in this set
+ *                   public int getIndividual( int i );               // Gets the value of the ith die in this set
+ *                   public String toString();                        // Returns a stringy representation of this set of dice
+ *                   public static String toString( DiceSet ds );     // Classwide version of the preceding instance method
+ *                   public boolean isIdentical( DiceSet ds );        // Returns true iff this set is identical to the set ds
+ *                   public static void main( String[] args );        // The built-in test program for this class
+ *
+ *  Notes         :  Stolen from Dr. Dorin pretty much verbatim, then modified to show some interesting
+ *                   things about Java, and to add this header block and some JavaDoc comments.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the number of sides or pips is out of range
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-09  B.J. Johnson  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class HighRoll{
+
+   public static void main( String args[] ) {
+      System.out.println( "\n   Welcome to the MainProgLoopDemo!!\n" );
+      System.out.println( "     Press the 'q' key to quit the program." );
+
+     // This line uses the two classes to assemble an "input stream" for the user to type
+     // text into the program
+      BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+      while( true ) {
+         System.out.print( ">>" );
+         String inputLine = null;
+         try {
+            inputLine = input.readLine();
+            if( 0 == inputLine.length() ) {
+               System.out.println("enter some text!:");
+            }
+            System.out.println( "   You typed: " + inputLine );
+            if( 'q' == inputLine.charAt(0) ) {
+               break;
+            }
+         }
+         catch( IOException ioe ) {
+            System.out.println( "Caught IOException" );
+         }
+      }
+   }
+}
