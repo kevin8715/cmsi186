727bf45c9e776803113777c82d8e4d2cffd020d0
kevin8715 <ksolis01@att.net>
2018-03-27 00:09:34 -0700

    Debugging needed
    Probably broken.

--- homework05/SoccerSim.java
+++ homework05/SoccerSim.java
@@ -1,102 +1,134 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
  *  File name     :  SoccerSim.java
+
  *  Purpose       :  The main program for the SoccerSim class
+
  *  @see
+
  *  @author       :  Kevin Solis
- *  Date written  :  2017-02-28
+
+ *  Date written  :  2017-03-26
+
  *  Description   :  This class provides a bunch of methods which may be useful for the SoccerSim class
+
  *                   for Homework 4, part 1.  Includes the following:
+
   *
+
  *  Notes         :  None right now.  I'll add some as they occur.
+
  *  Warnings      :  None
+
  *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
  *  Revision Histor
+
  *  ---------------
+
  *            Rev      Date     Modified by:  Reason for change/modification
+
  *           -----  ----------  ------------  -----------------------------------------------------------
+
  *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 
+
+
+
 public class SoccerSim {
+
   /**
+   *
    *  Class field definintions go here
+   * 
    */
-   private final double MAX_TIME_SLICE_IN_SECONDS  = 1800.00;
-   private final double DEFAULT_TIME_SLICE_IN_SECONDS = 60.0;
-   private final double DEFAULT_EPSILON_VALUE      = 0.1;      // small value for double-precision comparisons
 
-   private static final double SECONDS_PER_TWELVE_HOURS = 43200;
-   private static final double DEGREES_PER_ROTATION = 360.0;
+   private final double MAX_Timer_SLICE_IN_SECONDS  = 1800.00;
 
-   private static double[] checkedTimeArgs;
-   private static double inputedTimeSlice;
-   private static double firstAngleShortcut;
-   private static double secondAngleShortcut;
+   private final double DEFAULT_Timer_SLICE_IN_SECONDS = 60.0;
+
+
+
    private static final double HALF_WIDTH_OF_FIELD = 200;
+
    private static final double HALF_HEIGHT_OF_FIELD = 200;
+
+
+   private static final Ball[] ballStorage;
+
+
+   private static double timeSlice;
+
+   private static boolean collusion = false;
+
+   private static boolean allBallsStopped = false;
+
+
 
   /**
    *  Constructor
    *  This just calls the superclass constructor, which is "Object"
    */
+
    public SoccerSim() {
+
       super();
+
    }
+
+
 
   /**
    *  Method to handle all the input arguments from the command line
-   *   this sets up the variables for the simulation
-   Note to self: create pole object
+   *   this sets up the variables for the simulatiom
    */
+
    public void handleInitialArguments( String args[] ) {
-    Ball pole = new Ball()
 
-     if( 0 == args.length ) {
-        System.out.println( "Please start over and enter at least two sets of ball argument on the command line." );
+     if( 9 <= args.length ) {
+
+        System.out.println( "Please start over and enter at least two sets of ball arguments and a Timer slice on the command line." );
+
         System.exit( 1 );
-      }
-      Time Time = new Time();
-      checkedTimeArgs = new double[3];
-      if ( args.length == 3 ) {
-        checkedTimeArgs[ 0 ] = Time.( args[ 0 ] );
-        checkedTimeArgs[ 1 ] = Time.validateTimeSliceArg( args[ 1 ] );
-        checkedTimeArgs[ 2 ] = Time.validateEpsilonArg( args[ 2 ] );
-      }
-       if ( args.length == 2 ) {
-          checkedTimeArgs[ 0 ] = Time.validateAngleArg( args[ 0 ] );
-          checkedTimeArgs[ 1 ] = Time.validateTimeSliceArg( args[ 1 ] );
-          checkedTimeArgs[ 2 ] = DEFAULT_EPSILON_VALUE;
-        }
-          else if ( args.length == 1 ) {
-            checkedTimeArgs[ 0 ] = Time.validateAngleArg( args[ 0 ] );
-            checkedTimeArgs[ 1 ] = DEFAULT_TIME_SLICE_IN_SECONDS;
-            checkedTimeArgs[ 2 ] = DEFAULT_EPSILON_VALUE;
-          } 
 
-      for(int i = 0; i < numberOfBalls; i++){
-        Ball ball= new Ball();
-        ballStorage = new Ball(numberOfBalls);
-      checkedTimeArgs = new double[4];
-      if ( args.length == 4 ) {
-        checkedTimeArgs[ 0 ] = Time.( args[ 0 ] );
-        checkedTimeArgs[ 1 ] = Time.validateTimeSliceArg( args[ 1 ] );
-        checkedTimeArgs[ 2 ] = Time.validateEpsilonArg( args[ 2 ] );
       }
-       if ( args.length == 3 ) {
-          checkedTimeArgs[ 0 ] = Time.validateAngleArg( args[ 0 ] );
-          checkedTimeArgs[ 1 ] = Time.validateTimeSliceArg( args[ 1 ] );
-          checkedTimeArgs[ 2 ] = DEFAULT_EPSILON_VALUE;
-        }
-          else if ( args.length == 2 ) {
-            checkedTimeArgs[ 0 ] = Time.validateAngleArg( args[ 0 ] );
-            checkedTimeArgs[ 1 ] = DEFAULT_TIME_SLICE_IN_SECONDS;
-            checkedTimeArgs[ 2 ] = DEFAULT_EPSILON_VALUE;
-          } 
+     if(1 == args.length%4){
+      System.out.println("Illegal number of arguments");
 
-      inputedTimeSlice = checkedTimeArgs[ 1 ];
+      System.exit( 1 );
+     }
+
+      Timer time = new Timer();
+      timeSlice = time.validateTimerSliceArg( args[ args.length-1 ] );
+      int numberOfBalls =  args.length/4;
+      ballStorage = new Ball[numberOfBalls+1];
+
+
+    for(int i = 0; i < numberOfBalls-1; i++){
+
+      Ball ball = new Ball();
+
+      ball.validateXPosition(args[0+4*i]);
+      ball.validateYPosition(args[1+4*i]);
+      ball.validateXVelocity(args[2+4*i]);
+      ball.validateYVelocity(args[3+4*i]);
+
+      ballStorage[i] = ball;
    }
+      Ball pole = new ball();
+      ball.validateXPosition(0);
+      ball.validateYPosition(0);
+      ball.validateXVelocity(0);
+      ball.validateYVelocity(0);
+      ballStorage[ballStorage.length-1]= pole;
+ }
+
+
 
   /**
    *  The main program starts here
@@ -104,29 +136,54 @@ public class SoccerSim {
    *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
    *  @param  args  String array of the arguments from the command line
    *                args[0] is the angle for which we are looking
-   *                args[1] is the time slice; this is optional and defaults to 60 seconds
+   *                args[1] is the Timer slice; this is optional and defaults to 60 seconds
    *                args[2] is the epsilon value; this is optional and defaults to 0.1        
    */       
+
    public static void main( String args[] ) {
+
       SoccerSim SoccerSim = new SoccerSim();
+
       SoccerSim.handleInitialArguments( args );
-      Time Time = new Time();
-      firstAngleShortcut = Math.abs((Time.getMinuteHandAngle() -DEGREES_PER_ROTATION  ) - Time.getHourHandAngle() ); 
-      secondAngleShortcut  = Math.abs((Time.getHourHandAngle() - DEGREES_PER_ROTATION  ) - Time.getMinuteHandAngle() );
-      System.out.println( "\nThe simulation is running with an angle of " + checkedTimeArgs[ 0 ] + " degrees and a time slice of " + checkedTimeArgs[ 1 ] + " seconds." );
-      System.out.println("\n");
-      //check for when the angle happens with simulation running
-      while( Time.getTotalSeconds() <= SECONDS_PER_TWELVE_HOURS ) {
-         if ( Time.getHandAngle() > ( checkedTimeArgs[ 0 ] - ( checkedTimeArgs[ 0 ] * checkedTimeArgs[ 2 ] ) ) && Time.getHandAngle() < ( checkedTimeArgs[ 0 ] + ( checkedTimeArgs[ 0 ] * checkedTimeArgs[ 2 ] ) ) ) {
-          System.out.print( "Sought angle of " + checkedTimeArgs[ 0 ] + " degrees at time: " + Time.toString() + "\n" ); 
-        }
-         if ( Time.getHandAngle() > ( firstAngleShortcut - ( firstAngleShortcut * checkedTimeArgs[ 2 ] ) ) && Time.getHandAngle() < ( firstAngleShortcut + ( firstAngleShortcut * checkedTimeArgs[ 2 ] ) ) ) {
-          System.out.print( "Sought angle of " + checkedTimeArgs[ 0 ] + " degrees at time: " + Time.toString() + "\n" );
-        }
-         if ( Time.getHandAngle() > ( secondAngleShortcut - ( secondAngleShortcut * checkedTimeArgs[ 2 ] ) ) && Time.getHandAngle() < ( secondAngleShortcut + ( secondAngleShortcut * checkedTimeArgs[ 2 ] ) ) ) {
-          System.out.print( "Sought angle of " + checkedTimeArgs[ 0 ] + " degrees at time: " + Time.toString() + "\n" );
-        } Time.tick();
+
+      Timer time = new Timer();
+
+      int collusionItemOne;
+      int collusionItemTwo;
+
+      while( time.getTotalSeconds() <= SECONDS_PER_TWELVE_HOURS ){
+      allBallsStopped = true;
+      for(int i = 0; i < numberOfBalls; i++){
+        System.out.println(ballStorage[i]);
+        ballStorage[i].accelerationMovement();
+        if(ballStorage[i].getxVelocity() != 0 || ballStorage[i].getyVelocity()) != 0){
+          allBallsStopped = false;
+          }
       }
+      for(int i = 0; i < numberOfBalls; i++){
+        for(int j = i; j < numberOfBalls; j++)]
+          if(Math.abs(Math.abs(ballStorage[i].getxPosition()) - Math.abs(ballStorage[j].getxPosition()) < 8.9) && Math.abs(Math.abs(ballStorage[i].getyPosition()) - Math.abs(ballStorage[j].getyPosition()) < 8.9)
+            collusionItemTwo = j;
+            collusion = true;
+          }
+      }
+
+        clock.tick();
+      }
+      if(collusion){
+        System.out.println("Collusion at location: "+ballStorage[collusionLocation].getxPosition()+ " for x and for y at "+ballStorage[collusionLocation].getxPosition());
+        System.exit(0);
+      }
+      if(allBallsStopped){
+        System.out.println("All balls are stopped."+time.toString());
+        System.exit(0);
+      }
+
+
+     
+
       System.exit( 0 );
+
    }
+
 }
